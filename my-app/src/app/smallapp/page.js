0106ecaa08b7fc60240371064ccd2f70abe7e668
b00144312 <136645209+NicolaeCasian@users.noreply.github.com>
2024-11-07 04:02:25 +0000

    started work on register page and api's

--- my-app/src/app/smallapp/page.js
+++ my-app/src/app/smallapp/page.js
@@ -1,7 +1,15 @@
 'use client'
 
 import * as React from 'react';
-
+import PropTypes from 'prop-types';
+import { createTheme } from '@mui/material/styles';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonIcon from '@mui/icons-material/Person';
+import HomeIcon from '@mui/icons-material/Home';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import DescriptionIcon from '@mui/icons-material/Description';
+import LayersIcon from '@mui/icons-material/Layers';
 import AppBar from '@mui/material/AppBar';
 
 import Box from '@mui/material/Box';
@@ -24,7 +32,8 @@ export default function MyApp() {
 
   const [showLogin, setShowLogin] = useState(false);
 
-  const [showDash, setShowDash] = useState(false);
+  //Adding Cart to the block 
+  const [showCart, setShowCart ] = useState(false);
 
   const [showFirstPage, setShowFirstPage] = useState(true);
 
@@ -37,18 +46,19 @@ export default function MyApp() {
 
       setShowLogin(true);
 
-      setShowDash(false)
+      setShowCart(false);
+
 
   }
 
 
-  function runShowDash(){
+  function runShowCart(){
 
     setShowFirstPage(false);
 
     setShowLogin(false);
 
-    setShowDash(true)
+    setShowCart(true);
 
    
 
@@ -61,11 +71,13 @@ function runShowFirst(){
 
   setShowLogin(false);
 
-  setShowDash(false)
+  setShowCart(false)
 
  
 
 }
+
+
 
 
   return (
@@ -77,7 +89,7 @@ function runShowFirst(){
       <AppBar position="static">
 
         <Toolbar>
-
+          
           <IconButton
 
             size="large"
@@ -98,15 +110,35 @@ function runShowFirst(){
 
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 
-            MyApp
+          Krispy Kreme
 
           </Typography>
 
-          <Button color="inherit" onClick={runShowFirst}>First</Button>
+          
+          <IconButton
+            color='inherit'
+            size='large'>
+            <HomeIcon onClick={runShowFirst}>
+            </HomeIcon>
+          </IconButton>
 
-          <Button color="inherit" onClick={runShowLogin}>Login</Button>
+          <IconButton
+            color='inherit'
+            size='large'>
+            <PersonIcon onClick={runShowLogin}>
+            </PersonIcon>
+          </IconButton>
 
-          <Button color="inherit" onClick={runShowDash}>Dashboard</Button>
+          
+          <IconButton // Shopping Cart Icon Button
+          color='inherit' 
+          size='	large'> 
+            <ShoppingCartIcon onClick={runShowCart}>
+             </ShoppingCartIcon>
+             </IconButton>
+         
+
+         
 
         </Toolbar>
 
@@ -148,7 +180,7 @@ function runShowFirst(){
 
 
 
-    {showDash &&
+    {showCart &&
 
           <Box component="section" sx={{ p: 2, border: '1px dashed grey'}}>
 
